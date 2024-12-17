5703a630b1034b764b33640e81171470083c228f
Devon Burton <devon.burton@live.com>
2024-12-16 21:27:45 -0500

    testing projects created in the process of learning C#

---
+++ arrayExample/Program.cs
@@ -0,0 +1,7 @@
+﻿string[] fraudulentOrderIDs = [ "A123", "B456", "C789"];
+
+Console.WriteLine($"First: {fraudulentOrderIDs[0]}");
+Console.WriteLine($"Second: {fraudulentOrderIDs[1]}");
+Console.WriteLine($"Third: {fraudulentOrderIDs[2]}");
+
+Console.WriteLine($"There are {fraudulentOrderIDs.Length} fraudulent orders");
---
+++ commentPractice/Program.cs
@@ -0,0 +1,18 @@
+﻿/* This program takes an input string reversed it and counts the numer of times the letter o appears in the string */
+
+string inputString = "The quick brown fox jumps over the lazy dog.";
+char[] charString = inputString.ToCharArray();
+Array.Reverse(charString);
+int letterCount = 0;
+
+foreach (char letter in charString) 
+{ 
+    if (letter == 'o') 
+    { 
+        letterCount++; 
+    } 
+}
+
+string newMessage = new String(charString);
+Console.WriteLine(newMessage);
+Console.WriteLine($"'o' appears {letterCount} times.");
